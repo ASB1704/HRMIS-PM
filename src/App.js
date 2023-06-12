9fcc6ca4d688bc062723ab62be634504dd14149f
KritiKanak <kritikanak16@gmail.com>
2023-06-13 00:10:07 +0530

    Self Appraisal Form added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { PR_Landing_page } from './PR_Landing_page';
 import APAR_Form from './APAR_Form';
+import SelfAppraisalForm from './Self_Apparisal_Form';
 
 
 function App() {
   return (
     <div className="App">
       {/* <PR_Landing_page/> */}
-      <APAR_Form/>
+      <SelfAppraisalForm/>
       
     </div>
   );
