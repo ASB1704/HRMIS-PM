6ec1fcbbd275c51d4a445c550d6502e77f00a096
Abhishek <akg02052002@gmail.com>
2023-06-13 12:31:04 +0530

    employee dashboard added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
-import { PR_Landing_page } from './PR_Landing_page';
-import APAR_Form from './APAR_Form';
-
+// import { PR_Landing_page } from './PR_Landing_page';
+// import APAR_Form from './APAR_Form';
+import Dashboard from '../src/emp_dashboard/Dashboard';
+import Navbar from '../src/emp_dashboard/Navbar';
+import Sidebar from '../src/emp_dashboard/Sidebar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 function App() {
   return (
     <div className="App">
       {/* <PR_Landing_page/> */}
-      <APAR_Form/>
-      
+      {/* <APAR_Form/> */}
+      <Navbar />
+      <div class="container-fluid" id="main">
+        <div class="row row-offcanvas row-offcanvas-left">
+          <Sidebar />
+          <Dashboard />
+        </div>
+      </div>
     </div>
   );
 }
